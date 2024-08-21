dfd0c0edf9bd4b2822761e16bd9b462651c2a1ca
walon <walon@code-philosophy.com>
2024-08-21 23:05:42 +0800

    - 修复代码注释的错误 - 包名改为 com.code-philosophy.dhe_demo

--- Assets/HotUpdate/Hello.cs
+++ Assets/HotUpdate/Hello.cs
@@ -20,7 +20,7 @@ public class Hello
         int n = 0;
 
         // 热更新前，此处代码从0开始，函数未发生变化，走AOT编译
-        // 热更新后，此处代码改成从1开始，函数未发生变化，走AOT编译
+        // 热更新后，此处代码改成从1开始，函数发生变化，走解释执行
 #if HOT_UPDATE_CHANGE
         Debug.Log("========= Run Changed BenchmarkTest");
         for (int i = 1; i < 10000000; i++)
@@ -32,7 +32,7 @@ public class Hello
             n = n * round + 1;
         }
         sw.Stop();
-        Debug.Log($"Test1 [{round}]: cost {sw.ElapsedMilliseconds}ms");
+        Debug.Log($"Test1 [{round}]: cost {sw.ElapsedMilliseconds}ms, n={n}");
         return n;
     }
 }
